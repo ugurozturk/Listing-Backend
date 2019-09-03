16e8c67b0e5bcc1e55a1a0827795fb04472451b1
Uğur Öztürk <mail@uozturk.com>
2019-09-03 23:39:53 +0300

    Repository mantığı konuldu

---
+++ src/UOzturk.Listing.Core/IRepositories/ISystemCreatedListRepository.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UOzturk.Listing.List;
+
+namespace UOzturk.Listing.IRepositories
+{
+    public interface ISystemCreatedListRepository : IRepository<SystemCreatedListEntity>
+    {
+        List<SystemCreatedListEntity> GetAllWithItems(int? ListTypeId);
+    }
+}
---
+++ src/UOzturk.Listing.EntityFrameworkCore/EntityFrameworkCore/Repositories/SystemCreatedListRepository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Abp.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UOzturk.Listing.IRepositories;
+using UOzturk.Listing.List;
+
+namespace UOzturk.Listing.EntityFrameworkCore.Repositories
+{
+    public class SystemCreatedListRepository : ListingRepositoryBase<SystemCreatedListEntity>, ISystemCreatedListRepository
+    {
+        public SystemCreatedListRepository(IDbContextProvider<ListingDbContext> dbContextProvider) : base(dbContextProvider)
+        {
+        }
+
+        /// <summary>
+        /// TODO: UĞUR  buranın test edilmesi gerekiyor.
+        /// </summary>
+        /// <param name="ListTypeId"></param>
+        /// <returns></returns>
+        public List<SystemCreatedListEntity> GetAllWithItems(int? ListTypeId)
+        {
+            var query = GetAll();
+
+            if (ListTypeId.HasValue)
+            {
+                query = query.Where(x => x.ListTypeId == ListTypeId.Value);
+            }
+
+            return query
+                .Include(x => x.ListType)
+                .Include(x => x.SystemCreatedListItemCollection)//.ThenInclude(cs=>cs.SystemCreatedList)
+                .ToList();
+        }
+    }
+}
