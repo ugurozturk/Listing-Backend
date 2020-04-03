9769a54385fca26ad39ea191597ce9d18c5035fb
Uğur Öztürk <mail@uozturk.com>
2020-04-04 00:26:58 +0300

    tag filtered query

--- src/UOzturk.Listing.Application/Service/UserCreatedListItemTagAppService.cs
+++ src/UOzturk.Listing.Application/Service/UserCreatedListItemTagAppService.cs
@@ -1,5 +1,7 @@
-﻿using Abp.Application.Services;
+﻿using System.Linq;
+using Abp.Application.Services;
 using Abp.Domain.Repositories;
+using Abp.Linq.Extensions;
 using UOzturk.Listing.List;
 
 namespace UOzturk.Listing.UserCreatedListItemTag
@@ -10,6 +12,12 @@ namespace UOzturk.Listing.UserCreatedListItemTag
             IRepository<UserCreatedListItemTagEntity, int> repository
             ) : base(repository)
         {
+        }
+
+        protected override IQueryable<UserCreatedListItemTagEntity> CreateFilteredQuery(UserCreatedListItemTagPagedRequestDto input){
+            return base.CreateFilteredQuery(input)
+                .WhereIf(input.Name != default, f => f.Name.Contains(input.Name))
+                .WhereIf(input.UserCreatedListItemId != default, f => f.UserCreatedListItemId == input.UserCreatedListItemId);
         }
     }
 }
